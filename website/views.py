2aa4c3b0e67fa32e081efad9b748dfcc2f91a223
Joanna Lim <m210068@e.ntu.edu.sg>
2022-12-07 16:54:05 +0800

    cleaning up unused codes

--- website/views.py
+++ website/views.py
@@ -1,10 +1,8 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note, Drawing
+from .models import Drawing
 from . import db
 import json
-from io import BytesIO
-
 
 views = Blueprint('views', __name__)
 
